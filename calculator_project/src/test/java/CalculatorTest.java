1713380c441286b466e1540f4c34f807d9ae9b4a
Ankitha c <ankithacraj2001@gmail.com>
2025-05-27 07:10:52 +0530

    modified function

--- calculator_project/src/test/java/CalculatorTest.java
+++ calculator_project/src/test/java/CalculatorTest.java
@@ -5,21 +5,30 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.add(2, 3));
+        int result = calc.add(2,3);
+        System.out.println("Addition result: "+result);
+        assertEquals(5,result);
     }
     @Test
     public void testsub() {
         Calculator calc = new Calculator();
-        assertEquals(1, calc.sub(4, 3));
+        int result = calc.sub(8,4);
+        System.out.println("Subtraction result: "+result)
+        assertEquals(4,result);
     }
     @Test
     public void testmul() {
         Calculator calc = new Calculator();
-        assertEquals(6, calc.mul(2, 3));
+        int result = calc.mul(2,3)
+        System.println.out("Multiplication result: "+result)
+        assertEquals(6,result);
+
     }
     @Test
     public void testdiv() {
         Calculator calc = new Calculator();
-        assertEquals(2, calc.div(8, 4));
+        int result = calc.mul(8,4)
+        System.println.out("Division result: "+result)
+        assertEquals(4,result);
     }
 }
