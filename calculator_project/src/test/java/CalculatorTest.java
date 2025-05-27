36d176adca3ee8e1d7bed79bdb06056acefa7d89
Ankitha c <ankithacraj2001@gmail.com>
2025-05-27 06:56:22 +0530

    modified

--- calculator_project/src/test/java/CalculatorTest.java
+++ calculator_project/src/test/java/CalculatorTest.java
@@ -10,16 +10,16 @@ public class CalculatorTest {
     @Test
     public void testsub() {
         Calculator calc = new Calculator();
-        assertEquals(1, calc.add(4, 3));
+        assertEquals(1, calc.sub(4, 3));
     }
     @Test
     public void testmul() {
         Calculator calc = new Calculator();
-        assertEquals(6, calc.add(2, 3));
+        assertEquals(6, calc.mul(2, 3));
     }
     @Test
     public void testdiv() {
         Calculator calc = new Calculator();
-        assertEquals(2, calc.add(8, 4));
+        assertEquals(2, calc.div(8, 4));
     }
 }
